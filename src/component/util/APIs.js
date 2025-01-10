2371587e32d3949fee827e9b5c21eff9da431b9c
Anshu <anshusinghofficial16@gmail.com>
2025-01-11 00:23:05 +0530

    first commit

---
+++ src/component/util/APIs.js
@@ -0,0 +1,3 @@
+export const Traft_Login_Api = "http://localhost:8000/login";
+export const Traft_Register_Api = "http://localhost:8000/register";
+export const Traft_Get_Api = "http://localhost:8000/getData";
